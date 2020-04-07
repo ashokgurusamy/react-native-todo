842737fa14ce2ca8a2f1fde8734b6683563e6de3
ashok <ashok.g@videoken.com>
2020-04-07 16:37:10 -0500

    #First - Commit

---
+++ tempData.js
@@ -0,0 +1,69 @@
+// Temporary Data until we connect with Firebase
+
+export default tempData = [
+  {
+    id: 1,
+    name: "Get a Job",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Prepare Essnetials",
+        completed: false
+      },
+      {
+        title: "Brush up fundamentals",
+        completed: true
+      },
+      {
+        title: "Build Resume",
+        completed: true
+      },
+      {
+        title: "Apply now",
+        completed: false
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: "Daily Routine",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "Wake at 10",
+        completed: false
+      },
+      {
+        title: "Brush Up",
+        completed: true
+      },
+      {
+        title: "Have breakfast",
+        completed: true
+      },
+      {
+        title: "Start Coding",
+        completed: true
+      }
+    ]
+  }
+  //   {
+  //     id: 3,
+  //     name: "Birthday Party",
+  //     color: "#595BD9",
+  //     todos: [
+  //       {
+  //         title: "Get Balloons",
+  //         completed: false
+  //       },
+  //       {
+  //         title: "Send Invitations",
+  //         completed: true
+  //       },
+  //       {
+  //         title: "Make Dinner Reservations",
+  //         completed: true
+  //       }
+  //     ]
+  //   }
+];
